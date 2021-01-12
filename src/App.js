d4e050db44f4935d982d73b29189edb4865fcb2c
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-12 12:19:53 -0500

    installed material-ui core and icons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Navbar";
 import Search from "./components/Search";
 import Wrapper from "./components/Wrapper";
 
