9f23b23d24480b7bafbff8718f4a620d5ae90afa
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-17 20:44:38 -0500

    refactored project and removed some of unused files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Navbar";
-import SearchForm from "./components/SearchForm";
 import Wrapper from "./components/Wrapper";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <Router basename="/employee-directory">
       <div className="App">
         <Header />
-        {/* <SearchForm /> */}
         <Wrapper />
       </div>
     </Router>
